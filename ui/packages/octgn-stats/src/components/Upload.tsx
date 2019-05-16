98d0cc6fddfb71c0582b7c8c22e5323b95e77f6a
Mihai Dinculescu <faithraven@gmail.com>
2019-05-16 16:40:09 +0100

    Upload WIP.

--- ui/packages/octgn-stats/src/components/Upload.tsx
+++ ui/packages/octgn-stats/src/components/Upload.tsx
@@ -2,6 +2,7 @@ import { Button, Grid, SvgIcon, Typography } from '@material-ui/core';
 import withStyles, { StyledComponentProps, StyleRulesCallback } from '@material-ui/core/styles/withStyles';
 import { CloudUploadOutlined } from '@material-ui/icons';
 import * as React from 'react';
+import Dropzone, { DropzoneRef } from 'react-dropzone';
 
 import { parseFilesToGames } from '../helpers/uploadHelpers';
 import { IUploadedGame } from '../models/UploadedGame';
@@ -31,6 +32,34 @@ const styles: StyleRulesCallback = (theme) => ({
 	},
 });
 
+const dropzone = {
+	alignItems: 'center',
+	backgroundColor: '#fafafa',
+	borderColor: '#eeeeee',
+	borderRadius: 2,
+	borderStyle: 'dashed',
+	borderWidth: 2,
+	color: '#bdbdbd',
+	display: 'flex',
+	flex: 1,
+	flexDirection: 'column' as any,
+	outline: 'none',
+	padding: '20px',
+	transition: 'border .24s ease-in-out',
+};
+
+const dropzoneAccept = {
+	borderColor: '#00e676',
+};
+
+const dropzoneActive = {
+	borderColor: '#2196f3',
+};
+
+const dropzoneReject = {
+	borderColor: '#ff1744',
+};
+
 const UploadBase = (props: StyledComponentProps) => {
 	const [games, setGames] = React.useState<IUploadedGame[] | null>(null);
 	const { classes } = props;
@@ -41,6 +70,8 @@ const UploadBase = (props: StyledComponentProps) => {
 		console.log(newGames);
 	};
 
+	const dropzoneRef = React.createRef<DropzoneRef>();
+
 	return (
 		<>
 			<Grid spacing={24} alignItems='center' justify='center' container={true} className={classes!.grid}>
@@ -48,22 +79,6 @@ const UploadBase = (props: StyledComponentProps) => {
 					<div className={classes!.header}>
 						<div className={classes!.block}>
 							<Typography variant='h6' gutterBottom={true}>OCTGN Stats</Typography>
-							<ul>
-								<li>
-									<p>Hit <strong>Upload</strong>.</p>
-								</li>
-								<li>
-									<p>Navigate to your OCTGN History folder (e.g. <strong>C:\Users\my_user\Documents\OCTGN\History</strong>).</p>
-								</li>
-								<li>
-									<p>
-										Select the games you want to upload.
-										<br/>
-										<strong>Note:</strong> For a game to succesfully upload you need both the
-										<strong>.o8h</strong> and <strong>.o8l</strong> files.
-									</p>
-								</li>
-							</ul>
 						</div>
 						<div>
 							<input
@@ -87,6 +102,51 @@ const UploadBase = (props: StyledComponentProps) => {
 							</label>
 						</div>
 					</div>
+				</Grid>
+				<Grid item={true} xs={12}>
+					<Dropzone ref={dropzoneRef}>
+						{({getRootProps, getInputProps, isDragAccept, isDragActive, isDragReject, isFocused}) => {
+							const style = React.useMemo(
+								() => ({
+									...(dropzone),
+									...(isDragAccept ? dropzoneAccept : {}),
+									...(isDragActive || isFocused ? dropzoneActive : {}),
+									...(isDragReject ? dropzoneReject : {}),
+								}),
+								[
+									isDragAccept,
+									isDragActive,
+									isDragReject,
+									isFocused,
+								]
+							);
+
+							return (
+								<div {...getRootProps({style})}>
+									<input {...getInputProps()} />
+									<ul>
+										<li>
+											<p>Hit <strong>Upload</strong>.</p>
+										</li>
+										<li>
+											<p>
+												Navigate to your OCTGN History folder (e.g. <strong>C:\Users\my_user\Documents\OCTGN\History</strong>).
+											</p>
+										</li>
+										<li>
+											<p>
+												Select the games you want to upload.
+												<br/>
+												<strong>Note:</strong> For a game to succesfully upload you need both the
+												<strong>.o8h</strong> and <strong>.o8l</strong> files.
+											</p>
+										</li>
+									</ul>
+									<p>Drag 'n' drop some files here, or click to select files</p>
+								</div>
+							);
+						}}
+					</Dropzone>
 				</Grid>
 				{games !== null ? games.length : ''}
 			</Grid>
